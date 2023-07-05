7b91496feb9d4583f23f6bb7f221d54920223354
Mohamed Ouyizme <mohouyizme@gmail.com>
2023-07-05 16:37:51 +0100

    Remove Auth0 from env config

--- src/env/server.mjs
+++ src/env/server.mjs
@@ -4,18 +4,8 @@ import { z } from 'zod'
 export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
-    AUTH0_SECRET: z.string().min(32),
-    AUTH0_BASE_URL: z.string().url(),
-    AUTH0_ISSUER_BASE_URL: z.string().url(),
-    AUTH0_CLIENT_ID: z.string().min(1),
-    AUTH0_CLIENT_SECRET: z.string().min(1),
   },
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
-    AUTH0_SECRET: process.env.AUTH0_SECRET,
-    AUTH0_BASE_URL: process.env.AUTH0_BASE_URL,
-    AUTH0_ISSUER_BASE_URL: process.env.AUTH0_ISSUER_BASE_URL,
-    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
-    AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
   },
 })
