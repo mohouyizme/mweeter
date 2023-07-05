30566d9a0bda64d84c0689258f0dac95ce49f467
Mohamed Ouyizme <mohouyizme@gmail.com>
2023-07-05 14:39:10 +0100

    Add logic to log out button

--- src/components/LogOutButton.tsx
+++ src/components/LogOutButton.tsx
@@ -2,11 +2,12 @@ import { Icons } from '@/components/Icons'
 
 export default function LogOutButton() {
   return (
-    <div className="-mx-4">
-      <button className="flex w-full items-center rounded-full px-4 py-3 text-lg font-medium transition duration-300 hover:bg-gray-200">
-        <Icons.LogOut size={24} className="mr-4" />
-        Log out
-      </button>
-    </div>
+    <a
+      href="/api/auth/logout"
+      className="-mx-4 flex items-center rounded-full px-4 py-3 text-lg font-medium ring-gray-200 transition duration-300 hover:bg-gray-200"
+    >
+      <Icons.LogOut size={24} className="mr-4" />
+      Log out
+    </a>
   )
 }
