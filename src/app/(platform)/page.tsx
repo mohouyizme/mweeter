f8cdfa96a3375b1938781e9aeeaf6078f3b17590
Mohamed Ouyizme <mohouyizme@gmail.com>
2023-07-05 11:57:43 +0100

    Add send mweet input

--- src/app/(platform)/page.tsx
+++ src/app/(platform)/page.tsx
@@ -1,8 +1,10 @@
 import MweetCard from '@/components/MweetCard'
+import SendMweet from '@/components/SendMweet'
 
 export default function HomePage() {
   return (
     <div className="space-y-8">
+      <SendMweet />
       <div className="space-y-4">
         <h2 className="text-xl font-bold">Latest mweets</h2>
         <div className="rounded-3xl border bg-white shadow-sm">
---
+++ src/components/SendMweet.tsx
@@ -0,0 +1,71 @@
+'use client'
+
+import { useRef } from 'react'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+import { Button } from '@/components/ui/Button'
+import useAutosizeTextArea from '@/hooks/useAutosizeTextArea'
+import { cn } from '@/lib/utils'
+
+const mweetSchema = z.object({
+  mweet: z.string().min(1).max(255),
+})
+
+type MweetSchema = z.infer<typeof mweetSchema>
+
+export default function SendMweet() {
+  const maxLength = 255
+
+  const textAreaRef = useRef<HTMLTextAreaElement | null>(null)
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { isValid },
+  } = useForm<MweetSchema>({
+    resolver: zodResolver(mweetSchema),
+  })
+  const { ref, ...rest } = register('mweet')
+  const mweet = watch('mweet')
+  const mweetLength = mweet?.length ?? 0
+
+  useAutosizeTextArea(textAreaRef.current, mweet)
+
+  const onSubmit: SubmitHandler<MweetSchema> = (data) => console.log(data)
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="group relative overflow-hidden rounded-3xl border bg-white shadow-sm">
+        <textarea
+          placeholder="What's on your mind"
+          className="block w-full p-6 pb-24 text-xl outline-none placeholder:text-gray-500"
+          rows={1}
+          {...rest}
+          ref={(e) => {
+            ref(e)
+            textAreaRef.current = e
+          }}
+        />
+        <Button
+          size="xlarge"
+          rounded
+          disabled={!isValid}
+          className="absolute bottom-6 left-6"
+        >
+          Send mweet
+        </Button>
+        <span
+          className={cn(
+            'absolute bottom-6 right-6 text-sm font-bold',
+            maxLength - mweetLength <= 0 ? 'text-red-400' : 'text-gray-500'
+          )}
+        >
+          {maxLength - mweetLength}
+        </span>
+      </div>
+    </form>
+  )
+}
