ffff5c88cd3852c1ff2bf2573d9febcee888a1f4
Mohamed Ouyizme <mohouyizme@gmail.com>
2023-07-06 16:53:40 +0100

    Add follow and unfollow logic

--- src/app/api/users/[username]/follow/route.ts
+++ src/app/api/users/[username]/follow/route.ts
@@ -33,12 +33,136 @@ export async function GET(
       },
     })
 
-    const isFollowing = targetUser?.following?.length ?? 0 > 0
-    const isFollowedBy = targetUser?.followers?.length ?? 0 > 0
+    const isFollowedBy = (targetUser?.following?.length ?? 0) > 0
+    const isFollowing = (targetUser?.followers?.length ?? 0) > 0
+
+    console.log(isFollowedBy, isFollowing)
 
     return NextResponse.json({
       status: {
         isFollowing,
+        isFollowedBy,
+      },
+    })
+  } catch (error) {
+    return new Response('Something went wrong', { status: 500 })
+  }
+}
+
+export async function POST(
+  request: Request,
+  { params }: { params: { username: string } }
+) {
+  const user = await currentUser()
+  const username = params.username
+
+  if (user?.username === username)
+    return NextResponse.json({
+      status: {
+        isMyself: true,
+      },
+    })
+
+  const targetUser = await prisma.user.findUnique({
+    where: { username },
+    select: {
+      id: true,
+      followers: {
+        where: { followerId: user?.id },
+        select: { followerId: true },
+      },
+      following: {
+        where: { followingId: user?.id },
+        select: { followingId: true },
+      },
+    },
+  })
+
+  const isFollowedBy = (targetUser?.following?.length ?? 0) > 0
+  const isFollowing = (targetUser?.followers?.length ?? 0) > 0
+
+  if (isFollowing) {
+    return NextResponse.json({
+      status: {
+        isFollowing: true,
+        isFollowedBy,
+      },
+    })
+  }
+
+  try {
+    await prisma.follow.create({
+      data: {
+        followerId: user?.id as string,
+        followingId: targetUser?.id as string,
+      },
+    })
+
+    return NextResponse.json({
+      status: {
+        isFollowing: true,
+        isFollowedBy,
+      },
+    })
+  } catch (error) {
+    return new Response('Something went wrong', { status: 500 })
+  }
+}
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: { username: string } }
+) {
+  const user = await currentUser()
+  const username = params.username
+
+  if (user?.username === username)
+    return NextResponse.json({
+      status: {
+        isMyself: true,
+      },
+    })
+
+  const targetUser = await prisma.user.findUnique({
+    where: { username },
+    select: {
+      id: true,
+      followers: {
+        where: { followerId: user?.id },
+        select: { followerId: true },
+      },
+      following: {
+        where: { followingId: user?.id },
+        select: { followingId: true },
+      },
+    },
+  })
+
+  const isFollowedBy = (targetUser?.following?.length ?? 0) > 0
+  const isFollowing = (targetUser?.followers?.length ?? 0) > 0
+
+  if (!isFollowing) {
+    return NextResponse.json({
+      status: {
+        isFollowing: false,
+        isFollowedBy,
+      },
+    })
+  }
+
+  try {
+    await prisma.follow.delete({
+      where: {
+        followerId_followingId: {
+          followerId: user?.id as string,
+          followingId: targetUser?.id as string,
+        },
+      },
+    })
+
+    return NextResponse.json({
+      status: {
+        isFollowing: false,
         isFollowedBy,
       },
     })
--- src/components/FollowButton.tsx
+++ src/components/FollowButton.tsx
@@ -1,8 +1,11 @@
-import useSWR from 'swr'
+import { useState } from 'react'
+
+import useSWR, { mutate } from 'swr'
 
 import { fetcher } from '@/lib/fetcher'
 import { Follow } from '@/types/follow'
 
+import { Icons } from './Icons'
 import { Button } from './ui/Button'
 
 interface FollowButtonProps {
@@ -14,6 +17,27 @@ export default function FollowButton({ username }: FollowButtonProps) {
     `/api/users/${username}/follow`,
     fetcher
   )
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  async function followUser() {
+    setIsSubmitting(true)
+    await fetch(`/api/users/${username}/follow`, {
+      method: 'POST',
+    })
+    await mutate(`/api/users/${username}/follow`)
+    setIsSubmitting(false)
+    mutate(`/api/users/${username}?with_mweets=true`)
+  }
+
+  async function unfollowUser() {
+    setIsSubmitting(true)
+    await fetch(`/api/users/${username}/follow`, {
+      method: 'DELETE',
+    })
+    await mutate(`/api/users/${username}/follow`)
+    setIsSubmitting(false)
+    mutate(`/api/users/${username}?with_mweets=true`)
+  }
 
   if (isLoading)
     return (
@@ -29,8 +53,16 @@ export default function FollowButton({ username }: FollowButtonProps) {
 
   if (data?.status.isFollowing)
     return (
-      <Button variant="secondary" rounded className="absolute right-6 top-10">
-        Unfollow
+      <Button
+        onClick={() => unfollowUser()}
+        variant="secondary"
+        rounded
+        className="absolute right-6 top-10"
+      >
+        {isSubmitting ? (
+          <Icons.Loader size={14} className="mr-1 animate-spin" />
+        ) : null}
+        {isSubmitting ? 'Unfollowing' : 'Unfollow'}
       </Button>
     )
 
@@ -42,8 +74,16 @@ export default function FollowButton({ username }: FollowButtonProps) {
     )
 
   return (
-    <Button variant="primary" rounded className="absolute right-6 top-10">
-      Follow
+    <Button
+      onClick={() => followUser()}
+      variant="primary"
+      rounded
+      className="absolute right-6 top-10"
+    >
+      {isSubmitting ? (
+        <Icons.Loader size={14} className="mr-1 animate-spin" />
+      ) : null}
+      {isSubmitting ? 'Following' : 'Follow'}
     </Button>
   )
 }
